a8d2dc8397a3f977422fabc822ea5a165c23cd87
Iron Tuzki <759741749@qq.com>
2023-07-03 14:02:52 +0800

    mybatis x generator

---
+++ src/main/java/com/yupi/springbootinit/service/ChartService.java
@@ -0,0 +1,13 @@
+package com.yupi.springbootinit.service;
+
+import com.yupi.springbootinit.model.entity.Chart;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author Iron Tuzki
+* @description 针对表【chart(图表信息表)】的数据库操作Service
+* @createDate 2023-07-03 13:57:18
+*/
+public interface ChartService extends IService<Chart> {
+
+}
